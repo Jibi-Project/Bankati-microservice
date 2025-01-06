c86e0dd93301c5d49c0c02a23c410b8bb2b03110
btissam naciri <btissamnaciri70@gmail.com>
2025-01-06 15:15:34 +0100

    commit notif server pom 2

--- EcarteService/src/main/java/com/EcarteService/controller/ECarteController.java
+++ EcarteService/src/main/java/com/EcarteService/controller/ECarteController.java
@@ -8,6 +8,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
+@CrossOrigin(origins = "http://localhost:4200")
+
 
 @RestController
 @RequestMapping("/api/ecarte")
---
+++ notifications-server/src/test/java/com/example/notifications_server/NotificationsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.notifications_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationsServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
